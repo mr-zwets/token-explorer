4ba4d6b79757297d30d7c36ed250971aca38cdb1
Mathieu Geukens <mr-zwets@protonmail.com>
2025-01-19 09:49:31 +0100

    update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { BCMR, utf8ToBin, sha256, binToHex } from 'mainnet-js'
 import { useEffect, useState } from 'react'
