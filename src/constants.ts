5aacca9532f6ae318c4406e4b31c08888c55f687
Mathieu Geukens <mr-zwets@protonmail.com>
2026-05-11 12:10:33 +0200

    make chaingraph url environment variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
-export const CHAINGRAPH_URL = "https://gql.chaingraph.pat.mn/v1/graphql";
+// The pat.mn fallback can time out on heavier queries (e.g. queryIssuingUtxos
+// for tokens with many UTXOs), which surfaces as missing reserved-supply data.
+// Override via NEXT_PUBLIC_CHAINGRAPH_URL to point at a more powerful instance.
+export const CHAINGRAPH_URL =
+  process.env.NEXT_PUBLIC_CHAINGRAPH_URL ?? "https://gql.chaingraph.pat.mn/v1/graphql";
 export const IPFS_GATEWAY = "https://w3s.link/ipfs/";
 export const BLOCK_EXPLORER_URL = "https://explorer.bch.ninja/tx/";
 export const OTR_REGISTRY_URL = "https://otr.cash/.well-known/bitcoin-cash-metadata-registry.json";
